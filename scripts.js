f74a5ea1d294e3ee8ef263ab42731bd3d6832a63
John Sposato <jantspo@gmail.com>
2015-08-06 13:24:56 -0700

    finsihed object page

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
-function Car(make, model, year, numOfDoors, engineType){
-	this.make = make;
-	this.model = model;
-	this.numOfDoors = numOfDoors;
-	this.engineType = engineType;
+var myObject = {
+	firstProperty: "A string",
+	secondProperty: "Another string",
+	thirdProperty: 6,
+	fourthProperty: [
+		'value 1', 'value 2', 'value 3'
+	]
+}
+
+for(var prop in myObject){
+	console.log("Object " + prop + ' = ' + myObject[prop]);
 }
